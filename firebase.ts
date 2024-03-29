06a0999579bfdc6bceb09436cf5bf09602e6f5f5
Sunil Bista <106586662+SunilBista2001@users.noreply.github.com>
2024-03-29 14:02:40 +0545

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // initialization
 const firebaseConfig = {
-  apiKey: "AIzaSyCwHo99ReuQWQGahKmANIOOxYOp8z09E_w",
-  authDomain: "nextjs-chat-app-6ab88.firebaseapp.com",
-  projectId: "nextjs-chat-app-6ab88",
-  storageBucket: "nextjs-chat-app-6ab88.appspot.com",
-  messagingSenderId: "431493590910",
-  appId: "1:431493590910:web:369a65977c953a74000d2f",
-  measurementId: "G-RCS28GZLHN",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
